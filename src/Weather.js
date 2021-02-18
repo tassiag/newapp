52054704efdf5a4683df0ce2d6b968c50941ee42
tassiag <tassigomes@gmail.com>
2021-02-18 13:14:07 +0000

    add footer

--- src/Weather.js
+++ src/Weather.js
@@ -52,6 +52,14 @@ return (
             <button>Search</button>
         </form>
         <div>{message}</div>
+
+        <footer>
+            <p><a href="https://github.com/tassiag/newapp" target="_blank">Open-source</a> | Coded by <a href="https://www.linkedin.com/in/tassiagomes/" target="_blank">Tássia Gomes</a></p>
+        </footer>
     </div>
+
+      
+        
+
 );
 }
